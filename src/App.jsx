3263ec67e934dd382cd2d125ec9b60825a838b58
Naem Bhuiyan <hopingboynaem@gmail.com>
2018-09-23 03:13:49 +0600

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,23 @@ class App extends Component {
   state = {
     cards: [
       {
-        name: "Humayun",
+        name: "Gal Gadot",
         imageURL: "https://images.indianexpress.com/2018/01/gal-gadot-759.jpg"
       },
       {
-        name: "Naem",
+        name: "Emma Watson",
         imageURL:
           "https://static.arageek.com/wp-content/uploads/2017/09/Emma-Watson-1.jpg"
       },
       {
-        name: "Tamim",
+        name: "Deepika Padukoe",
         imageURL:
           "https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/06/28/Pictures/_b6e8e466-7aba-11e8-98f1-ed5cda4a01f0.jpg"
+      },
+      {
+        name: "Priyanka Chopra",
+        imageURL:
+          "https://filmfare.wwmindia.com/content/2018/sep/thu1537259674.jpg"
       }
     ]
   };
